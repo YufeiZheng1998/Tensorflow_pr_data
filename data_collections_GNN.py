f89717b769680ce9989f80c59e86c2d6c34a4f2d
Yufei Zheng <yufeiz2@andrew.cmu.edu>
2022-12-04 16:02:01 -0800

    change data collection script for GNN

--- data_collections_GNN.py
+++ data_collections_GNN.py
@@ -1,68 +1,75 @@
 import requests
 from github import Github
-from github.GithubException import RateLimitExceededException
+from github.GithubException import RateLimitExceededException, UnknownObjectException
 import csv
 import logging
 import calendar
 import time
 
-csv_columns = ['pr_id','pr_title','pr_body', 'requester', 'pr_commenters', 'pr_comments', 'original_reviewers', 'active_reviewers', 'assignees', 'commits_messages','changed_files','created_at']
+csv_columns = ['pr_id','pr_title','pr_body', 'requester', 'pr_commenters', 'pr_comments', 'active_reviewers', 'commits_messages','changed_files']
 csv_file = "tensorflow_sample.csv"
-g = Github("YOUR_GITHUB_TOKEN")
+g = Github("YOUR_GITHUBTOKEN")
 repo = g.get_repo("tensorflow/tensorflow")
-closed_prs = iter(repo.get_pulls(state='closed'))
 with open(csv_file, 'a+') as csvfile:
-  cnt = 0
+  pr_number = 1
   writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
   writer.writeheader()
   while True:
     try:
-      pr = next(closed_prs)
-      cnt += 1
-      print("Start Processing PR: {}".format(cnt))
+      print("Start Processing PR: {}".format(pr_number))
       pr_data = {}
+      pr = repo.get_pull(pr_number)
+      if pr.state != 'closed':
+        pr_number += 1
+        continue
+      pr_number += 1
+      if pr_number == 40000:
+        break
+      # Processing active reviewers
+      # Reviewer whose review status is "COMMENTED", "APPROVED" or 'CHANGES_REQUESTED'
+      reviews = pr.get_reviews()
+      active_reviewers = set()
+      for review in reviews:
+        if review.state == 'COMMENTED' or review.state == 'APPROVED' or review.state == 'CHANGES_REQUESTED':
+          active_reviewers.add(review.user.login)
+      # If no one reviews this pr, skip it.
+      if len(active_reviewers) == 0:
+        continue
+      pr_data['active_reviewers'] = list(active_reviewers)
+      
+      # Processing pr related basic information
       pr_data['pr_id'] = pr.number
       pr_data['pr_title'] = pr.title
       pr_data['pr_body'] = pr.body
       pr_data['requester'] = pr.user.login
+
+      
       r_comments = pr.get_review_comments()
       i_comments = pr.get_issue_comments()
       all_commenters = set()
       comment_docs = []
-      
+      # If the commenter is not in the list of active_reviewer, just skip.
       for r_comment in r_comments:
         if r_comment.user != None and 'bot' not in r_comment.user.login:
           reviewer = r_comment.user.login
+          if reviewer not in active_reviewers:
+            continue
           comment_docs.append(r_comment.body)
           all_commenters.add(reviewer)
       
       for i_comment in i_comments:
         if i_comment.user != None and 'bot' not in i_comment.user.login:
           reviewer = i_comment.user.login
-          all_commenters.add(reviewer)
+          if reviewer not in active_reviewers:
+            continue
           comment_docs.append(i_comment.body)
+          all_commenters.add(reviewer)
 
       pr_data['pr_commenters'] = list(all_commenters)
       pr_data['pr_comments'] = comment_docs
-      # TODO: find reviewers list
-      reviews = pr.get_reviews()
-      original_reviewers = []
-      active_reviewers = []
-      for review in reviews:
-        original_reviewers.append(review.user.login)
-        if review.state == 'COMMENTED' or review.state == 'APPROVED' or review.state == 'CHANGES_REQUESTED':
-          active_reviewers.append(review.user.login)
 
-      pr_data['original_reviewers'] = original_reviewers
-      pr_data['active_reviewers'] = active_reviewers
-      pr_assignees = []
-      for assignee in pr.assignees:
-        pr_assignees.append(assignee.login)
-      pr_data['assignees'] = pr_assignees
-      
-      # Process changed files(Probably do not need this one)..
-      # pr_data['changed_files_content'] = requests.get(pr.diff_url).content
-      # Commmit Messages
+
+      # Commmit Messages and changed_file_paths
       commits_messages = []
       commits_comments = []
       changed_files= set()
@@ -73,12 +80,8 @@ with open(csv_file, 'a+') as csvfile:
           changed_files.add(f.filename)
       pr_data['commits_messages'] = commits_messages
       pr_data['changed_files'] = list(changed_files)
-      # Add time related field
-      pr_data['created_at'] = pr.created_at
       writer.writerow(pr_data)
-    except StopIteration:
-      print("finished!!!")
-      break
+
     except RateLimitExceededException:
       search_rate_limit = g.get_rate_limit().search
       logging.info('search remaining: {}'.format(search_rate_limit.remaining))
@@ -87,4 +90,6 @@ with open(csv_file, 'a+') as csvfile:
       logging.info("Hit Rate Limit: sleep {}".format(sleep_time))
       time.sleep(sleep_time)
       continue
-
+    except UnknownObjectException:
+      pr_number += 1
+      continue
